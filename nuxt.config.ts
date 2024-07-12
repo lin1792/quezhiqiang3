c1889f18d3a4d1a7636a49febb6efbc00291ec45
linbinhui <1792048892@qq.com>
2024-07-12 09:23:09 +0800

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,24 +3,30 @@ export default defineNuxtConfig({
   // Nuxt 4 directory structure and features
   // https://nuxt.com/docs/getting-started/upgrade#testing-nuxt-4
   future: { compatibilityVersion: 4 },
+
   // Nuxt Modules
   // https://nuxt.com/modules
   modules: [
     '@nuxthub/core',
     '@nuxt/eslint'
   ],
+
   hub: {
     database: true,
     kv: true,
     blob: true,
     cache: true,
   },
+
   nitro: {
     experimental: {
       // Enable Server API documentation within NuxtHub
       openAPI: true
     }
   },
+
   // Development
   devtools: { enabled: true },
+
+  compatibilityDate: '2024-07-09',
 })
--- server/api/redirects/index.put.ts
+++ server/api/redirects/index.put.ts
@@ -1,8 +1,10 @@
 export default eventHandler(async (event) => {
   const body = await readBody(event)
+console.log(JSON.stringify(event));
+console.log(JSON.stringify(body));
 
   // Used in server/routes/redirects.ts
-  await hubKV().set('redirects', body)
+  await hubKV().set('redirects', {a:'s'})
 
   return body
 })
